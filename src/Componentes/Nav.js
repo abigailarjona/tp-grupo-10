05e9e8406e0f96da3d897a4e5cb1e5a7ebc28841
JazSallis <jazsallis@gmail.com>
2022-12-07 16:16:11 -0300

    Ajustes nav

--- src/Componentes/Nav.js
+++ src/Componentes/Nav.js
@@ -11,18 +11,34 @@ function Nav() {
       }
     });
     return () => {
-      window.removeEventListener("scroll",null);
+      window.removeEventListener("scroll", null);
     };
   }, []);
 
   return (
     <div className={`nav ${show && "nav_blank"}`}>
-      <img
-        className="nav_logo"
-        src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"
-        alt="Netflix Logo"
-      />
-      <img className="nav__avatar" src="images/avatar-7.png" alt="avatar" />
+      <div className="cont-nav">
+      <a href="#">
+        <img
+          className="nav_logo"
+          src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"
+          alt="Netflix Logo"
+        />
+        </a>
+
+        <div className="desplegable">
+          <button id="boton-invisible">
+            <a href="#">
+              <i className="fa-solid fa-user nav__avatar"></i>
+            </a>
+          </button>
+          <div className="links">
+            <a href="#">Mis Favoritos</a>
+            <hr/>
+            <a href="#">Log Out</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
