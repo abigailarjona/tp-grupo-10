5fdc6d7f533d1e21e2d6ccbe5ed95ccbbead8038
Mariano Munarriz <mmunarriz@gmail.com>
2022-12-09 15:43:03 -0300

    2fix_authcontext

--- src/Componentes/Logout.js
+++ src/Componentes/Logout.js
@@ -1,4 +1,4 @@
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/authContext";
 
 export function Logout() {
     const { logout, user } = useAuth();
--- src/Componentes/TopRated.js
+++ src/Componentes/TopRated.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
 import MovieCard from "./MovieCard";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/authContext";
 
 function TopRated() {
   const { user } = useAuth();
