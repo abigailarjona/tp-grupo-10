18cac68df87b90f014c17fca6c5ff50d422c31d9
Emiliano <juanemilianocicirello@gmail.com>
2022-12-10 20:04:23 -0300

    Formulario de contacto

--- src/Componentes/Contacto.js
+++ src/Componentes/Contacto.js
@@ -6,9 +6,9 @@ function Contacto() {
 
 
     return <form action="https://formsubmit.co/juanemilianocicirello@gmail.com" method="POST">
-                <input type="text" name="name" required placeholder="Nombre" className="name p-2" />
-                <input name="email" placeholder="Email" className="email p-2" type="email" required />
-                <textarea rows="4" cols="50" name="message" placeholder="Please enter your message" className="message p-2" required></textarea>
+                <input type="text" name="name" required placeholder="Nombre" className="name" />
+                <input name="email" placeholder="Email" className="email" type="email" required />
+                <textarea rows="4" cols="50" name="message" placeholder="Please enter your message" className="message" required></textarea>
                 
                 <input name="submit" className="btn p-2" type="submit"  />
             </form>
